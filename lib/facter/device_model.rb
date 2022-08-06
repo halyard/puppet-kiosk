8c0119e73f6380cde33bd2f50503a016beeed750
Les Aker <me@lesaker.org>
2022-08-06 12:44:13 -0400

    switch to confine

--- lib/facter/device_model.rb
+++ lib/facter/device_model.rb
@@ -1,6 +1,9 @@
 Facter.add('device_model') do
+  confine do
+    File.exist? '/sys/firmware/devicetree/base/model'
+  end
+
   setcode do
-    return nil unless File.exist? '/sys/firmware/devicetree/base/model'
     File.read('/sys/firmware/devicetree/base/model')
   end
 end
--- lib/facter/is_raspberry_pi.rb
+++ lib/facter/is_raspberry_pi.rb
@@ -1,6 +1,9 @@
 Facter.add('is_raspberry_pi') do
+  confine do
+    File.exist? '/sys/firmware/devicetree/base/model'
+  end
+
   setcode do
-    return false unless File.exist? '/sys/firmware/devicetree/base/model'
     File.read('/sys/firmware/devicetree/base/model') =~ /^Raspberry/
   end
 end
