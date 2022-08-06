5e7c1d6a0660424953ae36b5ff58bc854fec0d6e
Les Aker <me@lesaker.org>
2022-08-06 12:39:11 -0400

    update with guard clause

--- lib/facter/device_model.rb
+++ lib/facter/device_model.rb
@@ -1,5 +1,6 @@
 Facter.add('device_model') do
   setcode do
+    return nil unless File.exist? '/sys/firmware/devicetree/base/model'
     File.read('/sys/firmware/devicetree/base/model')
   end
 end
--- lib/facter/is_raspberry_pi.rb
+++ lib/facter/is_raspberry_pi.rb
@@ -1,5 +1,6 @@
 Facter.add('is_raspberry_pi') do
   setcode do
+    return false unless File.exist? '/sys/firmware/devicetree/base/model'
     File.read('/sys/firmware/devicetree/base/model') =~ /^Raspberry/
   end
 end
